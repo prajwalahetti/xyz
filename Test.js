03a70096eb6dc5c1e5bef0da8abb2c2dde7dc79f
Prajwal Ahetti <56022795+prajwalahetti@users.noreply.github.com>
2024-07-18 18:19:33 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,36 +1,27 @@
-sd
+// Function to create a map from json2 array
+function createMapFromJson2(json2) {
+    let map = {};
+    json2.forEach(obj => {
+        Object.keys(obj).forEach(key => {
+            map[key] = obj[key];
+        });
+    });
+    return map;
+}
 
-// Sample input JSON objects
-const json1 = {
-    "key1": "uniqueValue1",
-    "nested": {
-        "key2": "uniqueValue2"
-    },
-    "array": [
-        {"key3": "uniqueValue3"},
-        {"key4": "uniqueValue4"}
-    ]
-};
-
-const json2 = {
-    "uniqueValue1": "newValue1",
-    "uniqueValue2": {
-        "subKey": "subValue"
-    },
-    "uniqueValue3": ["arrayValue1", "arrayValue2"],
-    "uniqueValue4": "newValue4"
-};
-
-// Function to replace values in json1 with values from json2
-function replaceValues(json1, json2) {
+// Function to replace values in json1 with values from json2 map
+function replaceValues(json1, json2Map) {
     // Helper function to recursively traverse and replace values in the nested JSON
-    function traverseAndReplace(obj, json2) {
+    function traverseAndReplace(obj, json2Map) {
         for (let key in obj) {
+            // Check if the current value is an object and not null
             if (typeof obj[key] === 'object' && obj[key] !== null) {
-                traverseAndReplace(obj[key], json2);
+                // Recursively process nested objects and arrays
+                traverseAndReplace(obj[key], json2Map);
             } else {
-                if (json2.hasOwnProperty(obj[key])) {
-                    obj[key] = json2[obj[key]];
+                // Replace value if it matches a key in json2 map
+                if (json2Map.hasOwnProperty(obj[key])) {
+                    obj[key] = json2Map[obj[key]];
                 }
             }
         }
@@ -40,12 +31,14 @@ function replaceValues(json1, json2) {
     let clonedJson1 = JSON.parse(JSON.stringify(json1));
 
     // Traverse and replace values
-    traverseAndReplace(clonedJson1, json2);
+    traverseAndReplace(clonedJson1, json2Map);
 
     return clonedJson1;
 }
 
-// Replace values and print the result
-let result = replaceValues(json1, json2);
-console.log(JSON.stringify(result, null, 2));
+// Create map from json2 array
+let json2Map = createMapFromJson2(json2);
 
+// Replace values and print the result
+let result = replaceValues(json1, json2Map);
+console.log(JSON.stringify(result, null, 2));
