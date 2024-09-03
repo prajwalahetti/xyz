6bc236044e565ce18ee14c803e915c80bd09c507
Prajwal Ahetti <56022795+prajwalahetti@users.noreply.github.com>
2024-09-03 19:13:27 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,6 +1,16 @@
 const generateRandomValues = (numValues, totalSum) => {
+    if (numValues <= 1) throw new Error("numValues must be greater than 1");
+    if (totalSum <= numValues - 1) throw new Error("totalSum must be greater than numValues - 1");
+
     let values = Array.from({ length: numValues - 1 }, () => Math.floor(Math.random() * (totalSum - (numValues - 1))) + 1);
     values.push(totalSum - values.reduce((a, b) => a + b, 0));
+
+    // Ensure all values are positive
+    while (values.some(v => v <= 0)) {
+        values = Array.from({ length: numValues - 1 }, () => Math.floor(Math.random() * (totalSum - (numValues - 1))) + 1);
+        values.push(totalSum - values.reduce((a, b) => a + b, 0));
+    }
+
     return values;
 };
 
